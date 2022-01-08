78599f8cea9f7b08ad89f98ae4e92a82e1fcff43
bcroche <blas.croche@gmail.com>
2022-01-08 12:45:43 +0100

    version con colisiones y creación de enemigos
    Ejecutar con npm install y npm start

---
+++ src/game.js
@@ -0,0 +1,27 @@
+/*import Boot from './boot.js';
+import End from './end.js';
+import Level from './level.js';
+
+
+let config = {
+    type: Phaser.AUTO,
+    //Tamaño del canvas en pantalla
+    width:  800,
+    height: 600,
+    scale: {
+        // mode: Phaser.Scale.FIT,  
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+    },
+    pixelArt: true,
+    scene: [Boot, Level, End],
+    physics: { 
+        default: 'arcade', 
+        arcade: { 
+            gravity: { y: 400 }, 
+            debug: false 
+        } 
+    }
+};
+
+new Phaser.Game(config);
+*/
